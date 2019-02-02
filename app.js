e59e2adcac87d9aa531884e8140e73841f4f033b
William Martin <wjkmartin@gmail.com>
2019-02-02 08:36:30 -0800

    added buffer functionality

--- app.js
+++ app.js
@@ -1,38 +1,74 @@
-var d_punches = 44;
-var w_punches = 17;
+var config = {
+    apiKey: "AAAARBOssDY:APA91bF16wbUSOA-J8F1ZMEQ0B4Ml7OKTJK3D3YKD6tpeDvL0gAbVCvZbrK-0MDlQfOJlikXslRp4TJvi7eMVLx8FGw0BNXjL-xpnQW6_hpJTBrwheBLekJDF9dPjDB0tYlLBWm0b0oa",
+    databaseURL: "https://punch-buggy-e83c6.firebaseio.com/"
+  };
+  firebase.initializeApp(config);
 
-var dani = false;
-var will = false;
+var database = firebase.database();
+let punches 
 
-function update() {
-	document.getElementById("dani-progress").width = width(d_punches).toString() + '%';
-	document.getElementById("will-progress").width = width(w_punches).toString() + '%';
+let d_punches = 59;
+let w_punches = 30;
+
+let progressBarDani;
+let progressBarWill;
+
+let daniBuffer = 0;
+let willBuffer = 0;
+
+function appStart() {
+	
+	let _progressBarDani = document.getElementById("dani-progress");
+	let _progressBarWill = document.getElementById("will-progress");
+
+	progressBarDani = _progressBarDani;
+	progressBarWill = _progressBarWill;
+	update(_progressBarDani, _progressBarWill);
 }
 
-function width(n) {
-	return (n / 100) * 100;  
+function update() {
+	if (willBuffer > 0) {
+		document.getElementById("willPunches").style.color = "green";
+	}
+	else {
+		document.getElementById("willPunches").style.color = "black";
+	}
+
+	if (daniBuffer > 0) {
+		document.getElementById("daniPunches").style.color = "green";
+	}
+	else {
+		document.getElementById("daniPunches").style.color = "black";
+	}
+
+	document.getElementById("daniPunches").innerHTML = d_punches + daniBuffer;
+	document.getElementById("willPunches").innerHTML = w_punches + willBuffer;
+
+	progressBarDani.setAttribute("aria-valuenow", d_punches + daniBuffer);
+	progressBarWill.setAttribute("aria-valuenow", w_punches + willBuffer);
+
+	progressBarDani.setAttribute("style", 'width: ' + (d_punches + daniBuffer).toString() + '%')
+	progressBarWill.setAttribute("style", 'width: ' + (w_punches + willBuffer).toString() + '%');
+
 }
 
 function d_press() {
-	dani = true;
+	daniBuffer += 1;
+	update();
 }
 
 function w_press() {
-	will = true;
+	willBuffer += 1;
+	update();
 }
 
 function confirm() {
-	if (dani) {
-		d_punches++;
-	}
-	else if (will) {
-		w_punches++;
-	}
-	else {
-		return;
-	}
-	document.getElementById(id).width = width(d_punches);
-	document.getElementById(id).width = width(w_punches);
-	dani = false;
-	will = false;
+	d_punches += daniBuffer;
+	w_punches += willBuffer;
+
+	daniBuffer = 0;
+	willBuffer = 0;
+
+	update();
+	
 }
